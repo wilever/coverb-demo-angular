baba146f07b4082917332dd37162ba9747efb806
wilever <wilevergomez@gmail.com>
2018-11-30 20:33:34 -0300

    Add coverb demo v1.0.4

---
+++ src/main/webapp/app/coverb/default/component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'jhi-default',
+    templateUrl: './component.html',
+    styleUrls: ['style.scss']
+})
+export class DefaultComponent {}
