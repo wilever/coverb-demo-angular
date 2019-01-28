4460ee7c8e338ed58768270bdb8e8a03c4ee9ca4
jhipster-latino <wilevergomez@gmail.com>
2019-01-28 15:33:53 -0300

    update jhipster app to v5.8

---
+++ src/main/webapp/app/coverb/resume/component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import './icon_loader';
+
+@Component({
+    selector: 'jhi-resume',
+    templateUrl: './component.html',
+    styleUrls: ['style.scss']
+})
+export class ResumeComponent {}
